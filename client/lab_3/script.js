f18c2e1acb2367fe8206dfe3974e50977bd9e630
mbwillia88 <112646755+mbwillia88@users.noreply.github.com>
2022-09-30 17:36:59 -0400

    fixed prev button

--- client/lab_3/script.js
+++ client/lab_3/script.js
@@ -42,6 +42,11 @@ function moveToNextSlide() {
   updateSlidePosition(); // this is how you call a function within a function
 }
 function moveToPrevSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 3;
+  } else {
+    slidePosition -= 1;
+  }
   // add your code in here for when you click the "prev" button
   /*
     add an if statement here that checks
