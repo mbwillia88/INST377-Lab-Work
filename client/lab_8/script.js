8ebb9a02f21560b4d307dc1920dc3124f7062533
mbwillia88 <112646755+mbwillia88@users.noreply.github.com>
2022-11-18 17:10:17 -0500

    added markerPlace function to script

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -85,11 +85,21 @@ function filterList(array, filterInputValue) {
 
 function initMap() {
   console.log('innitMap');
-  const map = L.map('map').setView([51.505, -0.09], 13);
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  console.log('markerPlace', array);
+  // const marker = L.marker([51.5, -0.09]).addTo(map);
+  array.forEach((item) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[0], coordinates[1]]).addTo(map);
+  });
 }
 
 async function mainEvent() {
@@ -100,7 +110,7 @@ async function mainEvent() {
           If you separate your work, when one piece is complete, you can save it and trust it
       */
 
-  initMap();
+  const pageMap = initMap();
 
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
@@ -144,6 +154,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
+      markerPlace(currentList, pageMap);
     });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
@@ -158,6 +169,7 @@ async function mainEvent() {
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
+      markerPlace(currentList, pageMap);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
