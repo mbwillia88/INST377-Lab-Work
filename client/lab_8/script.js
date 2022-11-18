6edf7128849947bfd8274ece33d13722cd20c4d1
mbwillia88 <112646755+mbwillia88@users.noreply.github.com>
2022-11-18 16:43:16 -0500

    added tilelayer to script

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -83,9 +83,13 @@ function filterList(array, filterInputValue) {
   });
 }
 
-function initMap {
-    console.log('innitMap');
-    const map = L.map('map').setView([51.505, -0.09], 13);
+function initMap() {
+  console.log('innitMap');
+  const map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
 }
 
 async function mainEvent() {
@@ -96,9 +100,8 @@ async function mainEvent() {
           If you separate your work, when one piece is complete, you can save it and trust it
       */
 
-
   initMap();
-     
+
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
