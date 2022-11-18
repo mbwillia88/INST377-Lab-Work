2cf840b0982cccb26a307ff5e3d2d795f49c6aae
mbwillia88 <112646755+mbwillia88@users.noreply.github.com>
2022-11-18 17:16:59 -0500

    added the ability to remove markers

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -96,9 +96,15 @@ function initMap() {
 function markerPlace(array, map) {
   console.log('markerPlace', array);
   // const marker = L.marker([51.5, -0.09]).addTo(map);
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
   array.forEach((item) => {
     const {coordinates} = item.geocoded_column_1;
-    L.marker([coordinates[0], coordinates[1]]).addTo(map);
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
   });
 }
 
