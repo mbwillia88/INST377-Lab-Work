9f585d4df8d2678c4d68151b5b0cc70625f32b01
mbwillia88 <112646755+mbwillia88@users.noreply.github.com>
2022-11-18 17:29:02 -0500

    fixed bug

--- client/lab_8/script.js
+++ client/lab_8/script.js
@@ -94,7 +94,6 @@ function initMap() {
 }
 
 function markerPlace(array, map) {
-  console.log('markerPlace', array);
   // const marker = L.marker([51.5, -0.09]).addTo(map);
   map.eachLayer((layer) => {
     if (layer instanceof L.Marker) {
@@ -160,7 +159,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const filteredList = filterList(currentList, event.target.value);
       injectHTML(filteredList);
-      markerPlace(currentList, pageMap);
+      markerPlace(filteredList, pageMap);
     });
 
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
